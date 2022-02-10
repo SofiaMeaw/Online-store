8250f12c55bffca358c5f7a91d961e5cf628b967
SofiaMeaw <fatima.gomez@udgvirtual.udg.mx>
2022-02-10 17:12:29 -0600

    header

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header class="header">
+        <div class="header-img"/>
+        <menu>
+          <a class="home" href="/">Home</a>
+          <a class="cart" href="/">
+            <img src="https://i.pinimg.com/originals/15/4f/df/154fdf2f2759676a96e9aed653082276.png"/>
+          </a>
+          <a class="user" href="/">
+            <img src="https://static.thenounproject.com/png/1367727-200.png"/>
+          </a>
+        </menu>
+    </header>
+    </div>
+  );
+}
+
+export default App;
